c5c2e1d3936c2cdc9d6d2278fe25d2281be60ee0
pelli54 <pedro.emiliogl54@gmail.com>
2022-01-29 18:51:28 -0400

    Updates

---
+++ precache-manifest.e00a9a50c6d57ec8c079b382e5358587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5f0db9c07536a42bfe7a700700b58c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec252bf4bdc1f98577d",
+    "url": "/static/css/main.09c8b697.chunk.css"
+  },
+  {
+    "revision": "99bcda72a5b714a9404f",
+    "url": "/static/js/2.c4e94fc4.chunk.js"
+  },
+  {
+    "revision": "bec252bf4bdc1f98577d",
+    "url": "/static/js/main.326e4f50.chunk.js"
+  },
+  {
+    "revision": "301c53067c3a33529d78",
+    "url": "/static/js/runtime-main.39bd59cf.js"
+  }
+]);
---
+++ precache-manifest.e6cc4e0b4729c3101bec992988dae468.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dc531bdc5bc8afd1ae9230e8d97ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f0f0511da5bc6c7d381",
+    "url": "/static/css/main.09c8b697.chunk.css"
+  },
+  {
+    "revision": "99bcda72a5b714a9404f",
+    "url": "/static/js/2.c4e94fc4.chunk.js"
+  },
+  {
+    "revision": "9f0f0511da5bc6c7d381",
+    "url": "/static/js/main.f2cc8f94.chunk.js"
+  },
+  {
+    "revision": "301c53067c3a33529d78",
+    "url": "/static/js/runtime-main.39bd59cf.js"
+  }
+]);
